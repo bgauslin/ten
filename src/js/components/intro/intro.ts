62c8da10f380a05a4032fc94072f7e79e8b9a457
Ben Gauslin <github@gauslin.com>
2023-03-07 18:02:39 -0600

    simpler atom path with related refactoring

--- src/js/components/intro/intro.ts
+++ src/js/components/intro/intro.ts
@@ -80,20 +80,24 @@ class AppIntro extends LitElement {
     }
   }
 
-  // TODO: Simpler orbit paths. (Coordinate with CSS motion-path.)
   // TODO: Replace single nucleus with protons and neutons (with animations).
   renderAtom() {
+    const electrons = [];
+    for (let i = 0; i < 3; i++) {
+      electrons.push(html`
+        <div class="electron" id="electron-${i + 1}">
+          <div class="particle"></div>
+          <svg viewBox="0 0 200 200">
+            <path class="orbit" d="M10 100a90 30 0 1 0 180 0a90 30 0 1 0 -180 0 Z"/>
+          </svg>
+        </div>
+      `);
+    }
+
     return html`
       <div class="atom" aria-hidden="true">
-        <div class="atom__nucleus"></div>
-        <div class="atom__electron" id="electron-1"></div>
-        <div class="atom__electron" id="electron-2"></div>
-        <div class="atom__electron" id="electron-3"></div>
-        <svg viewBox="0 0 600 600">
-          <path d="M 384 299 C 384 151.539978 346.391907 32 300 32 C 253.608078 32 216 151.539978 216 299 C 216 446.460022 253.608078 566 300 566 C 346.391907 566 384 446.460022 384 299 Z"/>
-          <path d="M 342 371.746124 C 469.704102 298.016113 554.424744 205.676575 531.22876 165.5 C 508.032837 125.323425 385.704102 152.523865 258 226.253845 C 130.295868 299.983887 45.575256 392.323425 68.77121 432.5 C 91.967163 472.676575 214.295868 445.476135 342 371.746124 Z"/>
-          <path d="M 342 226.253845 C 214.295868 152.523865 91.967163 125.323425 68.77121 165.5 C 45.575256 205.676575 130.295868 298.016113 258 371.746124 C 385.704102 445.476135 508.032837 472.676575 531.22876 432.5 C 554.424744 392.323425 469.704102 299.983887 342 226.253845 Z"/>
-        </svg>
+        ${electrons}
+        <div class="nucleus"></div>
       </div>
     `;
   }
