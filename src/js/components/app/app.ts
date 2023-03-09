1af3615290faa7842a66474fc05cb642aefb08ca
Ben Gauslin <github@gauslin.com>
2023-03-09 07:47:53 -0600

    intro and popstate removed from scenes, moved to app; 1-based scenes index/attribute

--- src/js/components/app/app.ts
+++ src/js/components/app/app.ts
@@ -1,5 +1,5 @@
 import {LitElement, css, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, state} from 'lit/decorators.js';
 
 import shadowStyles from './app.scss';
 
@@ -8,12 +8,38 @@ import shadowStyles from './app.scss';
  */
 @customElement('ten-app')
 class App extends LitElement {
+  @state() play: boolean = true;
+  @state() popstateListener: EventListenerObject;
+  @state() scene: Number;
+
   static styles = css`${shadowStyles}`;
 
-  render() {    
+  constructor() {
+    super();
+    this.popstateListener = this.updateScene.bind(this);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('popstate', this.popstateListener, false);
+    this.updateScene();
+  }
+
+  disconnectedCallback() {
+    super.connectedCallback();
+    window.removeEventListener('popstate', this.popstateListener, false);
+  }
+
+  updateScene() {
+    const slug = window.location.pathname.split('/');
+    const scene = Number(slug[1]);
+    this.scene = (scene >= 1 && scene <= 42) ? scene : 1;
+  }
+
+  render() { 
     return html`
-      <ten-intro play></ten-intro>
-      <ten-scenes></ten-scenes>
+      <ten-intro ?play="${this.play}"></ten-intro>
+      <ten-scenes scene="${this.scene}"></ten-scenes>
     `;
   }
 }
--- src/js/components/scenes/scenes.ts
+++ src/js/components/scenes/scenes.ts
@@ -16,102 +16,47 @@ interface Scene {
  */
 @customElement('ten-scenes')
 class Scenes extends LitElement {
-  @property({attribute: 'scene', type: Number, reflect: true}) scene = 0;
-  @state() hasSlug: Boolean = false;
-  @state() intro?: HTMLElement;
-  @state() introObserver: MutationObserver;
-  @state() popstateListener: EventListenerObject;
-  @state() playing: boolean = false;
-  @state() ready: boolean = false;
+  @property({attribute: 'scene', type: Number, reflect: true}) scene = 1;
   @state() scenes: Scene[];
 
   static styles = css`${shadowStyles}`;
 
   connectedCallback() {
     super.connectedCallback();
-    this.fetchData();
-    this.watchIntro();
-    this.popstateListener = this.updateScene.bind(this);
-    window.addEventListener('popstate', this.popstateListener, false);
-  }
-
-  disconnectedCallback() {
-    super.connectedCallback();
-    window.removeEventListener('popstate', this.popstateListener, false);
-    this.introObserver.disconnect();
-  }
-
-  watchIntro() {
-    this.intro = document.querySelector('ten-intro');
-    if (this.intro) {
-      this.introObserver = new MutationObserver(this.waitForIntro.bind(this));
-      this.introObserver.observe(this.intro, {attributes: true});
-    }
-  }
-
-  waitForIntro(records: MutationRecord[]) {
-    for (const mutation of records) {
-      const {attributeName, target} = mutation;
-      this.playing = (<HTMLElement>target).hasAttribute('playing');
-      if (attributeName === 'playing' && !this.playing && !this.hasSlug) {
-        history.replaceState(null, '', '/1');
-      }
-    }
+    this.fetchData(); 
   }
 
   async fetchData(): Promise<Scene[]> {
-    if (this.ready) {
-      return;
-    }
-
     try {
       const response = await fetch('https://gauslin.com/api/ten/scenes.json');
       this.scenes = await response.json();
-      this.updateScene();
-      this.ready = true;
     } catch (error) {
       console.warn(error);
       return;
     }
   }
 
-  updateScene() {
-    const {pathname} = window.location;
-    const slugs = pathname.split('/');
-    const slug = Number(slugs[1]);
-
-    if (slug > 0 && slug <= this.scenes.length) {
-      this.scene = slug - 1;
-      this.hasSlug = true;
-    } else {
-      this.scene = 0;
-      const url = this.playing ? '/' : '/1';
-      history.replaceState(null, '', url);
-    }
-  }
-
   nextScene() {
     if (this.scene < this.scenes.length) {
       this.scene += 1;
-      this.updateWindow();
+      this.updateUrl();
     }
   }
 
   prevScene() {
-    if (this.scene > 0) {
+    if (this.scene > 1) {
       this.scene -= 1;
-      this.updateWindow();
+      this.updateUrl();
     }
   }
 
-  updateWindow() {
-    const slug = this.scene + 1;
-    history.pushState(null, '', slug.toString());
-    window.requestAnimationFrame(() => window.scrollTo(0, 0));
+  updateUrl() {
+    history.pushState(null, '', this.scene.toString());
+    window.requestAnimationFrame(() => window.scrollTo(0, 0)); // TODO
   }
 
   render() {
-    if (this.ready) {
+    if (this.scenes) {
       return html`
         ${this.renderScenes()}
         ${this.renderPrev()}
@@ -125,10 +70,11 @@ class Scenes extends LitElement {
       <ul>
       ${this.scenes.map((scene: Scene, index: number) => {
         const {blurb, distance, power} = scene;
+        const currentScene = this.scene - 1;
         return html`
           <li
-            aria-hidden="${index !== this.scene}"  
-            ?data-viewed="${index <= this.scene}">
+            aria-hidden="${index !== currentScene}"  
+            ?data-viewed="${index <= currentScene}">
             <img
               alt=""
               src="https://picsum.photos/600"
@@ -159,7 +105,7 @@ class Scenes extends LitElement {
         class="zoom"
         id="prev"
         title="Zoom out to previous scene"
-        ?disabled="${this.scene === 0}"
+        ?disabled="${this.scene === 1}"
         @click="${this.prevScene}">
         <svg viewbox="0 0 24 24" aria-hidden="true">
           <path d="M7,12 h10"/>
@@ -174,7 +120,7 @@ class Scenes extends LitElement {
         class="zoom"
         id="next"
         title="Zoom in to next scene"
-        ?disabled="${this.scene === this.scenes.length - 1}"
+        ?disabled="${this.scene === this.scenes.length}"
         @click="${this.nextScene}">
         <svg viewbox="0 0 24 24" aria-hidden="true">
           <path d="M6,12 h12 M12,6 v12"/>
