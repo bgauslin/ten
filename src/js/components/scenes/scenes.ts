5c8349338f9dd85157a067c2529bbc5419536883
Ben Gauslin <github@gauslin.com>
2023-03-15 10:36:26 -0500

    rewind button disabled until visible

--- src/js/components/scenes/scenes.ts
+++ src/js/components/scenes/scenes.ts
@@ -228,6 +228,7 @@ class Scenes extends LitElement {
         <button
           class="rewind"
           title="Rewind to the start"
+          ?disabled="${this.scene !== this.scenes.length}"
           @click="${this.rewindScenes}">
           <svg viewbox="0 0 24 24">
             <path d="M 12,6 L 6,12 L 12,18 Z" />
