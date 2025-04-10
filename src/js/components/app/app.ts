efebf20c431b812d40042809d18a10f1deeccb71
Ben Gauslin <github@gauslin.com>
2025-04-10 16:23:54 +0200

    removes unnecessary popstate listener

--- src/js/components/app/app.ts
+++ src/js/components/app/app.ts
@@ -8,9 +8,7 @@ import {customElement, state} from 'lit/decorators.js';
 @customElement('powers-of-ten')
 class App extends LitElement {
   private introListener: EventListenerObject;
-  private popstateListener: EventListenerObject;
   private replayListener: EventListenerObject;
-  private scenes = 42;
 
   @state() play: boolean = false;
   @state() ready: boolean = false;
@@ -20,7 +18,6 @@ class App extends LitElement {
     super();
     this.introListener = this.introDone.bind(this);
     this.replayListener = this.playIntro.bind(this);
-    this.popstateListener = this.playIntro.bind(this);
   }
 
   connectedCallback() {
@@ -29,7 +26,6 @@ class App extends LitElement {
     this.addEventListener('replay', this.replayListener);
     this.addEventListener('touchstart', this.handleTouchStart, {passive: true});
     this.addEventListener('touchend', this.handleTouchEnd, {passive: true});
-    window.addEventListener('popstate', this.popstateListener);
     this.playIntro();
   }
 
@@ -39,7 +35,6 @@ class App extends LitElement {
     this.removeEventListener('replay', this.replayListener);
     this.removeEventListener('touchstart', this.handleTouchStart);
     this.removeEventListener('touchend', this.handleTouchEnd);
-    window.removeEventListener('popstate', this.popstateListener);
   }
 
   protected createRenderRoot() {
@@ -51,11 +46,7 @@ class App extends LitElement {
   }
  
   private async playIntro() {
-    await this.updateComplete;
-
-    const segments = window.location.pathname.split('/');
-    const scene = parseInt(segments[segments.length - 1]);
-    this.play = scene > this.scenes || scene === 0 || isNaN(scene);
+    this.play = true;
   }
 
   private handleTouchStart(event: TouchEvent) {
